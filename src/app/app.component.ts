fae6d0ebb6bca21d865634c9702a71438767f3f6
amran-muhammad <amran8258@gmail.com>
2024-10-07 00:26:45 +0600

    youtube video from excel sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,23 @@ import { RouterOutlet } from '@angular/router';
 import { VoiceToTextComponent } from "./voice-to-text/voice-to-text.component";
 import { TextToVoiceComponent } from './text-to-voice/text-to-voice.component';
 import { CommonModule } from '@angular/common';
+import { YoutubeComponent } from './youtube/youtube.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, VoiceToTextComponent, TextToVoiceComponent,CommonModule],
+  imports: [RouterOutlet, VoiceToTextComponent, TextToVoiceComponent, YoutubeComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '';
+  youtube: string | null = "0";
+
+  constructor(){
+    if(localStorage.getItem("youtube")){
+      this.youtube = localStorage.getItem("youtube")
+    }
+  }
 }
 
---
+++ src/app/youtube/youtube.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-youtube',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './youtube.component.html',
+  styleUrl: './youtube.component.scss'
+})
+
+export class YoutubeComponent {
+  sheetData: Array<any> = [];
+  sheetUrl: string | null = '';
+  constructor(public sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.sheetUrl = 'https://script.google.com/macros/s/AKfycbwWs1reQyiCMSLMvqPDmhx4PCdizmR408If5IQAoBHinSrIMHZbmT4Z1yaqr3VvBIavWg/exec';
+    this.getData(this.sheetUrl)
+  }
+
+  async getData(url: string) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      this.sheetData = await response.json();
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+
+
+  getEmbedUrl(videoUrl: string): SafeResourceUrl {
+    const videoId = this.extractVideoId(videoUrl);
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+  }
+
+  extractVideoId(url: string): string {
+    const videoIdMatch = url.match(/(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/|v\/))([\w-]+)/);
+    return videoIdMatch ? videoIdMatch[1] : '';
+  }
+}
+
